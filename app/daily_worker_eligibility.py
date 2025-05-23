855d2f110a1012dc34c2f0caa04b7002c093c550
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-05-23 17:49:11 +0900

    Update daily_worker_eligibility.py

--- app/daily_worker_eligibility.py
+++ app/daily_worker_eligibility.py
@@ -50,7 +50,7 @@ def daily_worker_eligibility_app():
 
     worker_type = st.radio("근로자 유형을 선택하세요", ["일반일용근로자", "건설일용근로자"])
 
-    apply_date = st.date_input("수급자격 신청일을 선택하세요", value=datetime.today()).date()
+    apply_date = st.date_input("수급자격 신청일을 선택하세요", value=datetime.today())
     date_range = get_date_range(apply_date)
 
     st.markdown("---")
