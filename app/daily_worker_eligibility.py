64e50c38f3643d1e2dfc0b5d8513b521a47ed308
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-05-24 16:40:16 +0900

    Update daily_worker_eligibility.py

--- app/daily_worker_eligibility.py
+++ app/daily_worker_eligibility.py
@@ -175,8 +175,8 @@ def render_calendar_with_checkboxes(apply_date):
             min-width: 50px !important;
         }}
         div[data-testid="stCheckbox"] {{
-            width: 10px !important;
-            height: 45px !important;
+            width: 47px !important;
+            height: 47px !important;
         }}
         div[data-testid="stCheckbox"] label div[data-testid="stMarkdownContainer"] p {{
             font-size: 0.75rem !important;
