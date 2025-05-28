a4701b834e377abee070502a608aa739bff4c209
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-05-28 13:19:23 +0900

    Update realjob_application.py

--- app/realjob_application.py
+++ app/realjob_application.py
@@ -8,31 +8,17 @@ def realjob_application_app():
         </h3>
     """, unsafe_allow_html=True)
 
-    # 사이드바 (이미지 기반으로 메뉴 및 입력 필드 구성)
-    with st.sidebar:
-        st.markdown("### 실업인정")
-        st.text_input("구직번호", value="G10000H1EE")
-        st.text_input("구직일자", value="YYYY-MM-DD")
-        st.selectbox("사유확인", ["사유확인", "사유1", "사유2"])
-        st.markdown("### 구직활동")
-        st.text_input("PRBCON-SK 08")
-        st.text_input("~SOCK-SK 01")
-        st.markdown("### 연락처")
-        st.text_input("연락처", value="010-1234-5678")
-        st.markdown("### 기타")
-        st.text_input("입력값", value="입력값 예시")
-
-    # 메인 콘텐츠 (입력 부분 정렬 개선)
-    st.markdown("### 구직활동 확인")
+    # 구직활동 확인란
     col1, col2 = st.columns([1, 3])
     with col1:
         st.checkbox("구직활동이 없는 경우 체크하세요")
     with col2:
         st.button("구직활동 등록")
 
-    st.markdown("### 구직활동 정보 *")
+    # 구직활동 정보 입력
+    st.markdown("### 구직활동 *")
     with st.container():
-        col1, col2 = st.columns([1, 1])  # 열 너비 균등 조정
+        col1, col2 = st.columns([1, 1])
         with col1:
             st.text_input("구직번호 *", value="G10000H1EE-003")
             st.text_input("구직일자 *", value="YYYY-MM-DD")
@@ -41,6 +27,7 @@ def realjob_application_app():
             if st.button("사유체크를 입력하세요", key="reason_check"):
                 st.write("사유체크가 입력되었습니다.")
 
+    # 연락처/활동내역 입력
     st.markdown("### 연락처/활동내역 *")
     with st.container():
         col1, col2 = st.columns([1, 1])
@@ -48,19 +35,25 @@ def realjob_application_app():
             st.selectbox("연락처 *", ["010-1234-5678", "010-9876-5432"])
             st.radio("이메일 주소 입력", ["입력 안함", "입력함"])
             st.text_input("이메일", placeholder="이메일을 입력하세요")
-        with col2:
             st.selectbox("직업번호", ["직업을 선택하세요", "선택1", "선택2"])
+        with col2:
+            st.selectbox("사유확인", ["사유확인", "사유1", "사유2"])  # 이미지에서 추가된 필드
 
+    # 재취업 활동 입력
     st.markdown("### 재취업 활동 *")
     with st.container():
         col1, col2 = st.columns([1, 1])
         with col1:
             st.selectbox("활동 *", ["활동을 선택하세요", "활동1", "활동2"])
             st.selectbox("구체적 활동", ["구체적 활동을 선택하세요", "활동1", "활동2"])
+            st.selectbox("국적 발령", ["국적 발령", "옵션1", "옵션2"])  # 이미지에서 추가된 필드
         with col2:
             if st.button("모집 정보 입력", key="recruit_info"):
                 st.write("모집 정보가 입력되었습니다.")
+            if st.button("산학협력 입력하세요", key="industry_academia"):
+                st.write("산학협력 입력이 완료되었습니다.")  # 이미지에서 추가된 버튼
 
+    # 수급자격 입력
     st.markdown("### 수급자격 *")
     with st.container():
         col1, col2 = st.columns([1, 1])
