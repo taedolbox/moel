ddf4b909574ef0fe8c23a3973db2d08ecf219535
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-05-29 05:39:14 +0900

    Delete static/script.js

--- static/script.js
+++
@@ -1,6 +0,0 @@
-function toggleSidebar() {
-  const sidebar = document.querySelector('.stSidebar');
-  const button = document.querySelector('.toggle-btn');
-  sidebar.classList.toggle('collapsed');
-  button.textContent = sidebar.classList.contains('collapsed') ? '메뉴열기' : '메뉴닫기';
-}
