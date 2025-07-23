cd49b09d5a254a3d394e42279924230f1f6938eb
taedolbox <86582633+taedolbox@users.noreply.github.com>
2025-07-23 15:31:54 +0900

    Update daily_worker_eligibility.py

--- app/daily_worker_eligibility.py
+++ app/daily_worker_eligibility.py
@@ -460,6 +460,6 @@ def daily_worker_eligibility_app():
     </script>
     """
 
-    st.components.v1.html(calendar_html, height=1200, scrolling=False)
+    st.components.v1.html(calendar_html, height=1500, scrolling=False)
 
 
